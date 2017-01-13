e77fa76ac6b11a4562b2b4550042bb9ad06b0470
Delink-D <delinkdesigns@gmail.com>
2017-01-13 18:51:35 +0300

    To do list with Angular-2

---
+++ app/food-list.component.ts
@@ -0,0 +1,13 @@
+import { Component} from "@angular/core";
+import { Food } from './food.model';
+
+@Component ({
+	selector: 'foods',
+	template: `
+		
+	`
+})
+
+export class FoodComponent {
+	
+}
